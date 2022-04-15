4bfb457fbd174e71f9554b6e31e3b459163531c0
sysy-s <szymon.bold@gmail.com>
2022-04-15 23:04:24 +0200

    idk at this point

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "build")));
 
 // This route serves the React app
-app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(process.env.PORT || port, () => console.log(`Server listening on port ${port}`));
--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,3 @@
 // export const API = 'http://192.168.1.18:8000' // for local network
-// export const API = 'http://localhost:8000' // for dev
-export const API = 'https://palcelizacapi.herokuapp.com';
+export const API = 'http://localhost:8000' // for dev
+// export const API = 'https://palcelizacapi.herokuapp.com';
