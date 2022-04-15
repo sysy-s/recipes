e952bf651809618b1c52cfd6d9821ec6abef85f4
sysy-s <szymon.bold@gmail.com>
2022-04-16 01:23:56 +0200

    yess

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { HashRouter as Router } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+// hey
 ReactDOM.render(
   <Router>
     <Auth0Provider
