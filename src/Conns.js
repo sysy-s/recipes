a8db8b211dd0f1f6d9469252e47d27ababa3e054
sysy-s <szymon.bold@gmail.com>
2022-04-18 12:27:22 +0200

    small fix

--- src/Conns.js
+++ src/Conns.js
@@ -1,4 +1,4 @@
 // export const API = 'http://192.168.1.18:8000' // for local network
-export const API = 'http://localhost:8000' // for dev
-// export const API = 'https://palcelizacapi.herokuapp.com';
+// export const API = 'http://localhost:8000' // for dev
+export const API = 'https://palcelizacapi.herokuapp.com';
 export const AWS = process.env.REACT_APP_S3_BUCKET
